f1576e35add0b4f277208ccfdced8e64a76b4aa7
Tafadzwa <193275436+TmuD-05@users.noreply.github.com>
2025-05-19 07:06:33 +0200

    Add Static React Page usng Vite

---
+++ React-Static-Pages/learning react/src/header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+      <header>
+            <img src="/download.jpg" alt = "default" width="100px"/>
+            <nav >
+              <ul className='nav-list'>
+                <li> pricing </li>
+                <li> about </li>
+                <li> contact </li>
+              </ul>
+            </nav>
+          </header>
+    )}
