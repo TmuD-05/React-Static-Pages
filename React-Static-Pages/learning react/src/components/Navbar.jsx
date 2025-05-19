a806ffd4ee0b834133d843bef36f3cc6a1c99750
Tafadzwa <193275436+TmuD-05@users.noreply.github.com>
2025-05-19 08:13:25 +0200

    Add the Navigation bar
    Created the Navigation bar with the React logo
    Also added the Title of the website, same color and required font Inter

--- React-Static-Pages/learning react/src/components/Navbar.jsx
+++ React-Static-Pages/learning react/src/components/Navbar.jsx
@@ -1,5 +1,26 @@
+import './navbar.css';
 export default function Navigation(){
+    /**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
     return (
-        <h1> Nav bar</h1>
+        <>
+            <header className='navbody'>
+                <nav className='dis'>
+                    <img src="public/react-icon.jpeg" className='image'/>
+                    <h1 className="navig">
+                        ReactFacts 
+                    </h1>
+                </nav>
+            </header>
+        </>
+       
     )
 }
