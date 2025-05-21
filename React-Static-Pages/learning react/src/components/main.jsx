6a121a04172bd32ad592e2ec12ab299e1058428f
Tafadzwa <193275436+TmuD-05@users.noreply.github.com>
2025-05-21 16:31:28 +0200

    Add colour to bullets and background image

--- React-Static-Pages/learning react/src/components/main.jsx
+++ React-Static-Pages/learning react/src/components/main.jsx
@@ -2,16 +2,16 @@ import './main.css';
 export default function Main(){
     return (
         < >
-            <div className="main-body">
+            <main className="main-body">
                 <h1> Fun Facts about React </h1>
-                <ul>
+                <ul className='list'>
                     <li> Was released in 2013</li>
                     <li> Was originally created by Jordan Walke</li>
                     <li> has well over 100k stars on GitHub</li>
                     <li> Is maintained by Meta </li>
                     <li> Powers thousands of enterprise applications, including mobile apps</li>
                 </ul>
-            </div>
+            </main>
         </>
         
         
